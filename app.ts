db6afcdc9b7085447b9bfdbd9dae67bda4b3e849
evergara <ee-web@hotmail.com>
2018-02-17 00:22:53 -0500

    variables and data type

--- app.ts
+++ app.ts
@@ -3,5 +3,16 @@ function saludar(nombre){
 }
 
 var nombre = "Evergara";
+var resultado =saludar(nombre);
 
-document.getElementById("container").innerHtml=saludar(nombre);
+var etiqueta  = <HTMLElement>document.getElementById("container");
+etiqueta.innerHTML=resultado;
+
+
+//VARIABLES Y TIPOS DE DATOS
+var nombre:string = "Evergara";
+var edad:number = 38;
+var programado:boolean =true;
+var langs:Array<string> = ["JAVA","PHP","javascript","GO"];
+
+etiqueta.innerHTML=nombre + " "+ edad;
