6ca4fe85ee4cee2799bb0bdf59affea8312be9ea
evergara <ee-web@hotmail.com>
2018-02-19 23:33:28 -0500

    programacion poo

--- app.ts
+++ app.ts
@@ -16,3 +16,43 @@ var programado:boolean =true;
 var langs:Array<string> = ["JAVA","PHP","javascript","GO"];
 
 etiqueta.innerHTML=nombre + " "+ edad;
+
+//VARIABLES DE TIPO ANY(Permite meterle cualquier tipo de datos)
+var detalle:any ="Inteligente";
+
+etiqueta.innerHTML=nombre + " "+ edad + " "+detalle;
+//DIFERENCIA ENTRE LET Y VAR
+
+var a = 7;
+var b = 12;
+
+if(a==7){
+  let a = 4;
+  var b=1;
+  console.log(`Dentro del IF: var a =${a} y b=${b}`);
+}
+console.log(`Fuera del IF: var a =${a} y b=${b}`);
+//FUNCIONES Y TIPADOS
+function DevolverNumbert(num:number):string{
+  return num.toString();
+}
+console.log(DevolverNumbert(10));
+//POO
+
+//CLASES, PROPIEDADES Ó ATRIBUTOS Y METODOS
+
+//El CONTRUCTOR DE LA CLASE
+
+//VISIBILIDAD DE LAS PROPIEDADES
+
+//PROPIEDADES PRIVADAS
+
+//INTERFACES
+
+//HERENCIA
+
+//FUNCIONES DE FECHAS
+
+//DECORADORES
+
+//MODULOS
